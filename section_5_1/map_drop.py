375557c0519c2071b5f02b26a1fa2c40337d8683
Hunter Kippen <hunterkipt@gmail.com>
2023-07-04 15:34:34 -0400

    Fixed map_drop.py for SAGE 10.0

--- section_5_1/map_drop.py
+++ section_5_1/map_drop.py
@@ -8,21 +8,21 @@ def test_process(i, return_queue):
     time.sleep(4 * i + 1)
     return_queue.put(3 * i + 2)
 
+def ff(f, i, args, return_queue):
+    for trial in range(10):
+        r = f(i + 2**16 * trial, args)
+        return_queue.put(r)
+        return
+        # except:
+        #     pass
+        # print "FAILED 10 TIMES"
 
 def map_drop(nb, threads, f, aargs, max_drop=0):
     processes = []
 
-    def ff(i, args, return_queue):
-        for trial in range(10):
-            r = f(i + 2**16 * trial, args)
-            return_queue.put(r)
-            return
-            # except:
-            #     pass
-            # print "FAILED 10 TIMES"
 
     for i in range(threads):
-        process = Process(target=ff, args=(i, aargs, return_queue))
+        process = Process(target=ff, args=(f, i, aargs, return_queue))
         processes.append(process)
         process.start()
 
@@ -32,7 +32,7 @@ def map_drop(nb, threads, f, aargs, max_drop=0):
         if len(ret) == nb:
             break
         if i < nb + max_drop:
-            process = Process(target=ff, args=(i, aargs, return_queue))
+            process = Process(target=ff, args=(f, i, aargs, return_queue))
             processes.append(process)
             process.start()
 
